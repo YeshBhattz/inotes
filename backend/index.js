59a0700dacbac1d964edab646289273635e82f3c
YeshBhattz <86815567+YeshBhattz@users.noreply.github.com>
2025-07-19 10:16:55 +0530

    auth ans user model done

--- backend/index.js
+++ backend/index.js
@@ -4,10 +4,12 @@ const express = require('express')
 const app = express()
 const port = 3000
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+app.use(express.json())
+//these are the available routes:-
+app.use('/api/auth', require('./routes/auth'))
+app.use('/api/notes', require('./routes/notes'))
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
+ 
